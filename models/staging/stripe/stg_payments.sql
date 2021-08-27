4e9f6d6979441c4dcc26f9c23d85048f108906a6
Leo Folsom <leoebfolsom@gmail.com>
2021-08-27 02:43:54 +0000

    complete cents_to_dollars macro

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -3,7 +3,7 @@ select
         orderid as order_id,
         paymentmethod as payment_method,
         status,
-        {{ cents_to_dollars('amount') }} as amount,
+        {{ cents_to_dollars('amount', 3) }} as amount,
         created as created_at
 
     from {{ source('stripe', 'payment')}}
