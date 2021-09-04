fa6c2fab1a80ac2276cd48adfb8b845ab44e7fc4
Leo Folsom <leoebfolsom@gmail.com>
2021-09-04 03:04:21 +0000

    added db_utils macros, updated customers_daily_summary to have column name

--- models/customers_daily_summary.sql
+++ models/customers_daily_summary.sql
@@ -2,8 +2,8 @@ select
     {{ dbt_utils.surrogate_key(['customer_id', 'order_date']) }} as id,
     customer_id,
     order_date,
-    count(*)
+    count(*) AS customer_daily_orders
 
 from
     {{ ref('stg_orders') }}
-group by 1,2,3
+{{ dbt_utils.group_by(n=3) }}
